b64f333b42e759d390e7ea0490afd73e7cff41f5
margie <ly.margarett@gmail.com>
2015-07-28 13:29:28 -0600

    create loop to create routes for all objects

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -10,18 +10,16 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def find
-    respond_with Merchant.find_by(find_param.keys.first => find_param.values.first)
+    respond_with Merchant.find_by(find_param)
   end
 
   def find_all
     respond_with Merchant.where(find_param.keys.first => find_param.values.first)
   end
 
-# GET /api/v1/merchants/find_all?name=Cummings-Thiel
-
   private
 
     def find_param
-      params.reject { |key, value| key == "controller" || key == "action" }
+      params.select { |key, value| key == "name" }
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get '/merchants/random', to: "merchants#random"
-      get '/merchants/find', to: "merchants#find"
-      get '/merchants/find_all', to: "merchants#find_all"
-      get '/merchants/:id', to: "merchants#show"
+
+  model_objects = ["customers", "merchants", "items", "invoices", "invoice_items", "transactions"]
+
+  model_objects.each do |model_object|
+    namespace :api do
+      namespace :v1 do
+        get "/#{model_object}/random", to: "#{model_object}#random"
+        get "/#{model_object}/find", to: "#{model_object}#find"
+        get "/#{model_object}/find_all", to: "#{model_object}#find_all"
+        get "/#{model_object}/:id", to: "#{model_object}#show"
+      end
     end
   end
 end
