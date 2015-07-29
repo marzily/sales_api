cec768bb652ce450dbfee95e27cac9fac0e6c4a1
margie <ly.margarett@gmail.com>
2015-07-28 22:34:02 -0600

    create invoices api controller actions: transactions, invoice_items, items, customer, merchant

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -19,6 +19,26 @@ class Api::V1::InvoicesController < ApplicationController
     render json: Invoice.where(find_param)
   end
 
+  def transactions
+    render json: Invoice.find(params[:id]).transactions
+  end
+
+  def invoice_items
+    render json: Invoice.find(params[:id]).invoice_items
+  end
+
+  def items
+    render json: Invoice.find(params[:id]).items
+  end
+
+  def customer
+    render json: Invoice.find(params[:id]).customer
+  end
+
+  def merchant
+    render json: Invoice.find(params[:id]).merchant
+  end
+
   private
 
     def find_param
--- config/routes.rb
+++ config/routes.rb
@@ -2,31 +2,28 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+
+      # endpoints
       model_objects = ["customers", "merchants", "items", "invoices", "invoice_items", "transactions"]
       model_objects.each do |model_object|
-        get "/#{model_object}/random", to: "#{model_object}#random"
-        get "/#{model_object}/find", to: "#{model_object}#find"
+        get "/#{model_object}/random",   to: "#{model_object}#random"
+        get "/#{model_object}/find",     to: "#{model_object}#find"
         get "/#{model_object}/find_all", to: "#{model_object}#find_all"
-        get "/#{model_object}/:id", to: "#{model_object}#show"
-        get "/#{model_object}", to: "#{model_object}#index"
+        get "/#{model_object}/:id",      to: "#{model_object}#show"
+        get "/#{model_object}",          to: "#{model_object}#index"
       end
 
-      get "/merchants/:id/items", to: "merchants#items"
+      # merchant relationships
+      get "/merchants/:id/items",    to: "merchants#items"
       get "/merchants/:id/invoices", to: "merchants#invoices"
+
+      # invoice relationships
+      get "/invoices/:id/transactions", to: "invoices#transactions"
+      get "invoices/:id/invoice_items", to: "invoices#invoice_items"
+      get "/invoices/:id/items",        to: "invoices#items"
+      get "/invoices/:id/customer",     to: "invoices#customer"
+      get "/invoices/:id/merchant",     to: "invoices#merchant"
     end
   end
-
-# merchant items
-# merchant invoices
-
-  # def generate_endpoint_routes
-  #   model_objects = ["customers", "merchants", "items", "invoices", "invoice_items"]
-  #   model_objects.each do |model_object|
-  #     get "/#{model_object}/random", to: "#{model_object}#random"
-  #     get "/#{model_object}/find", to: "#{model_object}#find"
-  #     get "/#{model_object}/find_all", to: "#{model_object}#find_all"
-  #     get "/#{model_object}/:id", to: "#{model_object}#show"
-  #   end
-  # end
 
 end
