62a1455f114cb819c3723907db00a74829614492
margie <ly.margarett@gmail.com>
2015-07-28 22:05:08 -0600

    change transactions api controller to render json instead of using responders gem

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,16 +1,14 @@
 class Api::V1::TransactionsController < ApplicationController
-  respond_to :json
-
   def random
-    respond_with Transaction.random
+    render json: Transaction.random
   end
 
   def show
-    respond_with Transaction.find(params[:id])
+    render json: Transaction.find(params[:id])
   end
 
   def index
-    respond_with Transaction.all
+    render json: Transaction.all
   end
 
   def find
@@ -18,13 +16,17 @@ class Api::V1::TransactionsController < ApplicationController
   end
 
   def find_all
-    render json: Transaction.where(find_param.keys.first => find_param.values.first)
+    render json: Transaction.where(find_param)
   end
 
   private
 
     def find_param
       attributes = %w[id invoice_id credit_card_number credit_card_expiration_date result created_at updated_at]
-      params.select { |key, value| attributes.include?(key) }
+      attributes.each do |attribute|
+        if params.has_key?(attribute)
+          return { attribute.to_sym => params[attribute] }
+        end
+      end
     end
 end
