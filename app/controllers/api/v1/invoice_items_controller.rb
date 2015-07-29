3f66f70b0fa538fc684aedc3f73d54d93d327920
margie <ly.margarett@gmail.com>
2015-07-28 22:03:44 -0600

    create index action for invoice_items api, change invoice_items api controller to render json instead of using responders gem

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -1,26 +1,32 @@
 class Api::V1::InvoiceItemsController < ApplicationController
-  respond_to :json
-
   def random
-    respond_with InvoiceItem.random
+    render json: InvoiceItem.random
   end
 
   def show
-    respond_with InvoiceItem.find(params[:id])
+    render json: InvoiceItem.find(params[:id])
+  end
+
+  def index
+    render json: InvoiceItem.all
   end
 
   def find
-    respond_with InvoiceItem.find_by(find_param)
+    render json: InvoiceItem.find_by(find_param)
   end
 
   def find_all
-    respond_with InvoiceItem.where(find_param.keys.first => find_param.values.first)
+    render json: InvoiceItem.where(find_param)
   end
 
   private
 
     def find_param
       attributes = %w[id item_id invoice_id quantity unit_price created_at updated_at]
-      params.select { |key, value| attributes.include?(key) }
+      attributes.each do |attribute|
+        if params.has_key?(attribute)
+          return { attribute.to_sym => params[attribute] }
+        end
+      end
     end
 end
