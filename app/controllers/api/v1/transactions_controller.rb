261624b97292d326f64e55afa15dfd5b2c886e56
margie <ly.margarett@gmail.com>
2015-07-30 10:30:39 -0600

    update find param method for transactions controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -26,7 +26,7 @@ class Api::V1::TransactionsController < ApplicationController
   private
 
     def find_param
-      attributes = %w[id invoice_id credit_card_number credit_card_expiration_date result created_at updated_at]
+      attributes = %w[id invoice_id credit_card_number result created_at updated_at]
       attributes.each do |attribute|
         if params.has_key?(attribute)
           return { attribute.to_sym => params[attribute] }
