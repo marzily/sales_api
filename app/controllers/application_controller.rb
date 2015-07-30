547c2cbb9aa98ccd28012ae087ff80844c85a179
margie <ly.margarett@gmail.com>
2015-07-30 11:47:28 -0600

    made ApplicationController#find_param private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def find_param(attributes)
-    attributes.each do |attribute|
-      if params.has_key?(attribute)
-        return { attribute.to_sym => params[attribute] }
+  private
+    def find_param(attributes)
+      attributes.each do |attribute|
+        if params.has_key?(attribute)
+          return { attribute.to_sym => params[attribute] }
+        end
       end
     end
-  end
+
 end
